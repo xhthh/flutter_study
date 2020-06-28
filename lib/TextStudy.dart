8d1f70cca6d3ae046c39484267dc930fea3262e5
许海涛 <xuhaitao@autohome.com.cn>
2020-06-28 17:59:12 +0800

    Text Widget

---
+++ lib/TextStudy.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TextStudy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter  '),
+        ),
+        body: Center(
+          child: Text(
+            'Retrofit\nRetrofit充当一个适配器的角色，将一个Java接口翻译成Http请求，然后通过OkHttp去发送这个请求。',
+            textAlign: TextAlign.left,
+            maxLines: 5,
+//            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 255, 125, 125),
+//                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.solid),
+          ),
+        ),
+      ),
+    );
+  }
+}
