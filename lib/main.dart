d15721464a45355c2c882a296d9d1a9946ee5a8b
许海涛 <xuhaitao@autohome.com.cn>
2020-07-02 16:01:14 +0800

    本地assets图片，打包

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,17 @@ import 'package:flutter/material.dart';
 import 'module/navigation/NavigationTest3.dart';
 
 void main() {
-  runApp(NavigationTest3());
+  runApp(
+    MyApp()
+  );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset('images/meinv.jpg'),
+    );
+  }
+}
+
