86bceb1306f87dae462c54ce8bf19e059ab947dc
许海涛 <xuhaitao@autohome.com.cn>
2020-07-02 10:21:33 +0800

    导航

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,47 @@ import 'package:flutter/material.dart';
 import 'module/ContainerStudy.dart';
 
 void main() {
-  runApp(
-//    TextStudy()
-//    ContainerStudy()
-//    ImageStudy()
-//    ListViewVStudy()
-//    ListViewDStudy(items: List<String>.generate(20, (i) => "Item $i"))
-//    GridViewStudy()
-//    RowContainer()
-//    ColumnContainer()
-      CardContainer());
+  runApp(MaterialApp(
+    title: '导航演示01',
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('导航页面'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondScreen()));
+          },
+          child: Text('查看商品详情页'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品详情页'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('返回'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
 }
