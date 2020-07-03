a1d0fd5378e92fdcba2394f27cc9cbec6207738e
许海涛 <xuhaitao@autohome.com.cn>
2020-07-03 11:20:08 +0800

    tab选项卡

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutterstudy/pages/frosted_glass_demo.dart';
+import 'package:flutterstudy/pages/keep_alive_demo.dart';
 import 'package:flutterstudy/pages/pages.dart';
 
 void main() {
-  runApp(FrostedGlassTest());
+  runApp(KeepAliveTest());
 }
 
 
---
+++ lib/pages/keep_alive_demo.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class KeepAliveTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: KeepAliveDemo(),
+    );
+  }
+}
+
+class KeepAliveDemo extends StatefulWidget {
+  @override
+  _KeepAliveDemoState createState() => _KeepAliveDemoState();
+}
+
+class _KeepAliveDemoState extends State<KeepAliveDemo>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Keep Alive Demo'),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: <Widget>[
+          MyHomePage(),
+          MyHomePage(),
+          MyHomePage(),
+        ],
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with AutomaticKeepAliveClientMixin {
+  int _counter = 0;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('点一次增加一个数字'),
+            Text('$_counter', style: Theme.of(context).textTheme.display1)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
