31bb0f8eeca1928c41bf4bddabaacc45bef4a063
许海涛 <xuhaitao@autohome.com.cn>
2020-07-03 10:37:59 +0800

    动画效果

--- lib/pages/custome_router.dart
+++ lib/pages/custome_router.dart
@@ -17,9 +17,35 @@ class CustomRoute extends PageRouteBuilder {
                 Animation<double> animation1,
                 Animation<double> animation2,
                 Widget child) {
-              return FadeTransition(
-                opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-                    parent: animation1, curve: Curves.fastOutSlowIn)),
+              //渐隐渐现的动画效果
+//              return FadeTransition(
+//                opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+//                    parent: animation1, curve: Curves.fastOutSlowIn)),
+//                child: child,
+//              );
+              //缩放的动画效果
+//              return ScaleTransition(
+//                scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+//                    parent: animation1, curve: Curves.fastOutSlowIn)),
+//                child: child,
+//              );
+
+              //旋转+缩放动画效果
+//              return RotationTransition(
+//                turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+//                    parent: animation1, curve: Curves.fastOutSlowIn)),
+//                child: ScaleTransition(
+//                  scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+//                      parent: animation1, curve: Curves.fastOutSlowIn)),
+//                  child: child,
+//                ),
+//              );
+              //左右滑动动画
+              return SlideTransition(
+                position: Tween<Offset>(
+                        begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0))
+                    .animate(CurvedAnimation(
+                        parent: animation1, curve: Curves.bounceOut)),
                 child: child,
               );
             });
